58a7a37d0afedaa81b07cdc95304e32da6da418d
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-09 17:36:22 -0300

    Adicionando menu

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import { Home } from './components/Home'
 import { Sobre } from './components/Sobre'
 import { Alunos } from './components/Alunos'
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <h1>Aplicação React</h1>
       <BrowserRouter>
-      <ul>
-        <li> <Link to="/"> Pagina inicial </Link></li>
-        <li> <Link to="/alunos"> Cadastro de Alunos </Link></li>
-        <li> <Link to="/sobre">Sobre </Link></li>
-      </ul>
-      <Routes>
-        <Route path="/" index element={ <Home/> }></Route>
-        <Route path="/alunos" element={ <Alunos /> }></Route>
-        <Route path="/sobre" element={ <Sobre /> }></Route>
-      </Routes>
-    </BrowserRouter>
+
+      <Nav variant="tabs">
+        <Nav.Link as={Link} to="/">Pagina Inicial</Nav.Link>
+        <Nav.Link as={Link} to="/alunos">Cadastro de alunos</Nav.Link>
+        <Nav.Link as={Link} to="/sobre">Sobre</Nav.Link>
+      </Nav>
+
+        <Routes>
+          <Route path="/" index element={ <Home/> }></Route>
+          <Route path="/alunos" element={ <Alunos /> }></Route>
+          <Route path="/sobre" element={ <Sobre /> }></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
