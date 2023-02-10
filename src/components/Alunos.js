8173b2e89c730f7b5635bdebd6954e419125242a
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-10 15:28:32 -0300

    Criação tabela

--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -1,8 +1,34 @@
+import React from 'react'
+import { Table } from 'react-bootstrap';
 
-export function Alunos(){
+export class Alunos extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            alunos: []
+        }
+    }
+
+    render(){
     return(
-        <div>
-            <p1>Componente Alunos</p1>
-        </div>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Leandro</td>
+                    <td>leandro@gmail.com</td>
+                    <td>Atualizar / Excluir</td>
+                </tr>
+            </tbody>
+        </Table>
     )
+}
 }
