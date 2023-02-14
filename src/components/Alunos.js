ba5546977f36883f7b4bf8bbc259c6f3713dc4ef
leandrolinkolnr <leandro.rodrigues@aluno.uepb.edu.br>
2023-02-14 15:23:26 -0300

    cadastra Aluno

--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Table } from 'react-bootstrap';
+import { Button, Table, Form } from 'react-bootstrap';
 import axios from "axios";
 
 export class Alunos extends React.Component{
@@ -8,9 +8,10 @@ export class Alunos extends React.Component{
         super(props);
 
         this.state = {
-            alunos: [
-            ]
-        }
+            nome: '',
+            email: '',
+            alunos: []
+}
     }
 
     componentDidMount(){
@@ -30,7 +31,7 @@ export class Alunos extends React.Component{
         fetch("http://localhost:3000/alunos/"+id, {method: 'DELETE'})
         .then(resposta => {
             if(resposta.ok){
-                alert("Post deleted!");
+                alert("Aluno deletado com sucesso!");
                 this.buscarAluno();
             }
         })
@@ -46,10 +47,23 @@ export class Alunos extends React.Component{
           });
       } */
 
+    cadastraAluno = (aluno) => {
+        fetch("http://localhost:3000/alunos/", {method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(aluno)
+    })
+        .then(resposta => {
+            if(resposta.ok){
+                alert("Aluno cadastrado com sucesso!");
+                this.buscarAluno();
+            } else {
+                alert("Não foi possivel cadastrar o aluno!");
+            }
+        })
+    }
 
-    render(){
-    return(
-        <Table striped bordered hover>
+renderTabela(){
+    return  <Table striped bordered hover>
             <thead>
                 <tr>
                     <th>Nome</th>
@@ -70,6 +84,57 @@ export class Alunos extends React.Component{
 
             </tbody>
         </Table>
+}
+
+render()    {
+    return(
+        <div>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Nome</Form.Label>
+                <Form.Control type="text" placeholder="Digite o nome do aluno" value={this.aluno.nome} onChange={this.atualizaNome} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Digite o email do aluno" value={this.aluno.email} onChange={this.atualizaEmail}/>
+                </Form.Group>
+        
+                <Button variant="primary" type="submit" onClick={this.submit}>
+                    Adicionar
+                </Button>
+            </Form>
+
+            {this.renderTabela()}
+        </div>
     )
 }
+
+atualizaNome = (e) => {
+    this.setState(
+        {
+            nome: e.target.value
+        }
+    )
+}
+
+atualizaEmail = (e) => {
+    this.setState(
+        {
+            email: e.target.value
+        }
+    )
+}
+
+
+submit(){
+    const aluno = {
+        id:0,
+        nome: this.state.nome,
+        email: this.state.email
+    }
+
+    this.cadastraAluno(aluno);
+}
+
 }
